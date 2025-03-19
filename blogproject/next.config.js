6ff290ae90ae011a9ccea3d64a71908c73e9f9eb
Maichiee <imam.harits24@gmail.com>
2025-03-19 11:29:15 +0700

    Fix: Add Next.js dependency

---
+++ blogproject/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+  /* config options here */
+};
+
+module.exports = nextConfig;
